b7fab27225df93cdc0e4dcaa00019fee1d7660c9
rallerG <raller799@live.dk>
2018-04-21 12:26:35 +0200

    Working version

--- Asign3A.c
+++ Asign3A.c
@@ -16,9 +16,9 @@ int main(void) {
 
     printf("%s", "Insert the amount of roofs \n" );
     scanf("%d", &segNum);
-    while(!(1 <= segNum && segNum <=40000)){
+    if(!(1 <= segNum && segNum <=40000)){
         printf("%s\n", "There can be between 1 - 40000 segments" );
-        scanf("%d", &segNum);
+        return 0;
     }
 
     struct segment array[segNum];
@@ -39,14 +39,13 @@ int main(void) {
       }
     else {
       printf("%s\n", "Invalid input, try again");
+      return 0;
     }
       //Check if the values are correct x1 < x2 & y1 != y2
     }
 
     //------------------------------------------------------------------------------
 
-     // water < 0 = 0
-     // += i j
     printf("\n");
     }
 
@@ -55,7 +54,6 @@ int main(void) {
     }
 
     for(int i = 0; i < segNum; i++){
-      // array[i].water = array[i].x2 - array[i].x1;       //Look above
       for(int j = 0; j < segNum; j++){
           if (i != j) {
               if(overlaps(i, j, array[i], array[j]) ) {
@@ -70,34 +68,6 @@ int main(void) {
     return 0;
 }
 
-
-//##############################################################################
-//# I think the problem is that not all roofs have a value yet when trying to transfer the water
-//# Maybe initialize alle water values before trying to transfer
-//#
-//#
-//# Segment 3
-//# 5 5 9 3
-//# Water is 13
-//# Is off by 2
-//#
-//#
-//#
-//#
-//# Roof 1 7 5 6 is transfered before it has it own overlap calculated, so it
-//# transfers to much water
-//#
-//#
-//# Check if for water transfer whether it should be < or <= if x align
-//#
-//#
-//#
-//#
-//# Also needs to make sure that a partially covered roof only transfers water equal to the uncovered part
-//# Maybe change order roofs are processed from top to buttom, instead of first to last input
-//##############################################################################
-
-
 int overlaps(int i, int j, struct segment current, struct segment other) {
     int overlap = 0;
 
@@ -111,8 +81,7 @@ int overlaps(int i, int j, struct segment current, struct segment other) {
               //j   ---
               //i -------
               if(current.x1 < other.x1 && current.x2 > other.x2) {
-                  // overlap = other.water;
-                  overlap = 0;                      //@@@Since it doesn't matter what side the water falls
+                  overlap = (-other.water);      //Since it doesn't matter what side the water falls
                   return overlap;
               } else if(current.x1 > other.x1 && current.x2 < other.x2){
                   //j --------
@@ -122,25 +91,43 @@ int overlaps(int i, int j, struct segment current, struct segment other) {
               } else if(current.x1 > other.x1 && current.x2 > other.x2){
                   //j ------
                   //i     ------
-                      // overlap = other.x2 - current.x1;
-                      if (other.y1 > other.y2) {    //@@@
-                        printf("%s%d%s%d\n", "Current water er: ", current.water, " other water er: ", other.water );
+                      if (other.y1 > other.y2) {
                         overlap = (-other.water);
-                      } else {
-                        overlap = other.x2 - current.x1;
                       }
+                        overlap =  overlap + (other.x2 - current.x1);
                       return overlap;
               } else {
                   //j    ------
                   //i ------
-                      // overlap = current.x2 - other.x1;
                       if (other.y1 < other.y2) {
                         overlap = (-other.water);
-                      } else {
-                        overlap = current.x2 - other.x1;
                       }
+                        overlap = overlap + (current.x2 - other.x1);
                       return overlap;
               }
           }
       }
 }
+
+
+
+/*
+Givet opgave
+Roof 6
+13 7 15 6
+3 8 7 7
+1 7 5 6
+5 5 9 3
+6 3 8 2
+9 6 12 8
+
+
+Ekstra input
+Roof 5
+2 5 3 6
+4 7 7 6
+4 5 6 6
+0 4 5 5
+2 3 4 4
+
+*/
